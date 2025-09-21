1942d4bd873af5ede6b1078c4f14aca514da3624
Kush Patel <kushpatelj@Mac.lan>
2025-09-20 21:19:12 -0700

    added routing pages

---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app) 
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+
+
+@app.route('/login')
+def home():
+    return render_template('login.html')
+
+
+
+
+
+@app.route('/signup')
+def home():
+    return render_template('signup.html')
+
+
+
+@app.route('/profile')
+def home():
+    return render_template('profile.html')
+
+
+
+@app.route('/upload')
+def home():
+    return render_template('upload.html')
+
+
+@app.route('/albums')
+def home():
+    return render_template('albums.html')
+
+@app.route('/photo')
+def home():
+    return render_template('photo.html')
+
